0924f4931e63c9ec752fd152d1b841a1b8e5b43d
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-09-13 12:13:05 +0530

    adding parser

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import { render } from "react-dom";
-import Pet from "./Pet";
+import ReactDOM from "react-dom";
+
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+     <SearchParams/>
     </div>
   );
 };
 
-render(<App />, document.getElementById("root"));
-
-// You will be seeing a console warning  """"Warning: Each child in a list should have a unique "key" prop"""". in your browser console. React's dev warnings are trying to help your code run faster. Basically React tries to keep track of components are swapped in order in a list and it does that by you giving it a unique key it can track. If it sees two things have swapped, it'll just move the components instead of re-rendering.
+ReactDOM.render(<App />, document.getElementById("root"));
